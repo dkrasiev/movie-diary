76c1bacbc74c85f0dad95e0311d91b2f4dfe9da6
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-04-10 23:15:28 +0400

    add MovieSearch

---
+++ src/components/MovieSearch.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  function navigateToMovie(e: Event) {
+    const movieId = (e.target as HTMLInputElement).value;
+    if (!movieId) return;
+
+    window.location.href = `/movies/${movieId}`;
+  }
+</script>
+
+<input class="border rounded p-2" on:change={navigateToMovie} />
