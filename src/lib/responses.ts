c4cb778d9f44fb193ac3b27e60bc4a1758424105
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-04-13 23:47:28 +0400

    feat: add more responses

--- src/lib/responses.ts
+++ src/lib/responses.ts
@@ -1,13 +1,26 @@
-export const badRequestResponse = () =>
-  new Response("Bad request", { status: 400 });
-
-export const notFoundResponse = () =>
-  new Response("Not found", { status: 404 });
-
-export const jsonResponse = (obj: object, options: ResponseInit = {}) =>
-  new Response(JSON.stringify(obj), {
+// Successful responses
+export const jsonResponse = (body: object) =>
+  new Response(JSON.stringify(body), {
     headers: {
       "Content-Type": "application/json",
     },
-    ...options,
   });
+
+// Client error responses
+export const badRequestResponse = () =>
+  new Response("Bad Request", { status: 400 });
+
+export const unauthorizedResponse = () =>
+  new Response("Unauthorized", { status: 401 });
+
+export const forbiddenResponse = () =>
+  new Response("Forbidden", { status: 403 });
+
+export const notFoundResponse = () =>
+  new Response("Not Found", { status: 404 });
+
+export const conflictResponse = () => new Response("Conflict", { status: 409 });
+
+// Server error responses
+export const internalErrorResposne = () =>
+  new Response("Internal Error", { status: 500 });
