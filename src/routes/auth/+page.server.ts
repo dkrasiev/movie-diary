f259e117a1cc55dca8527feb2bd37d48904ffecb
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-04-20 22:39:52 +0400

    feat: add error handlers to auth

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -2,6 +2,7 @@ import { setTokenCookie } from '$lib/server/httpUtils/set-token-cookie.js';
 import authService from '$lib/server/services/auth.service.js';
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types.js';
+import { HttpError } from '$lib/server/httpUtils/http-error.js';
 
 export const actions = {
 	login: async ({ request, cookies }) => {
@@ -13,10 +14,16 @@ export const actions = {
 			return fail(400, { missing: true });
 		}
 
-		const { user, token } = await authService.login(email, password);
-		setTokenCookie(cookies, token);
+		try {
+			const { token } = await authService.login(email, password);
+			setTokenCookie(cookies, token);
 
-		return { user };
+			return { ok: true, result: 'Login completed successfully' };
+		} catch (e) {
+			if (e instanceof HttpError) {
+				return { error: { status: e.status, data: e.data } };
+			}
+		}
 	},
 
 	register: async ({ request, cookies }) => {
@@ -28,9 +35,15 @@ export const actions = {
 			return fail(400, { missing: true });
 		}
 
-		const { user, token } = await authService.register(email, password);
-		setTokenCookie(cookies, token);
+		try {
+			const { user, token } = await authService.register(email, password);
+			setTokenCookie(cookies, token);
 
-		return { user };
+			return { user };
+		} catch (e) {
+			if (e instanceof HttpError) {
+				return { error: { status: e.status, data: e.data } };
+			}
+		}
 	}
 } satisfies Actions;
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -3,20 +3,39 @@
 
 	export let form: ActionData;
 
-	let mode: 'login' | 'register' = 'login';
+	let authFormElement: HTMLFormElement;
+	let action: string = '?/login';
+
+	function send(action: '?/login' | '?/register') {
+		authFormElement.action = action;
+		authFormElement.submit();
+	}
 </script>
 
-<form class="flex flex-col" action="?/login" method="post">
+<form class="flex flex-col" {action} method="post" bind:this={authFormElement}>
 	<input name="email" type="email" required />
 	<input name="password" type="password" required />
-	<button>Log In</button>
+	<div class="flex justify-content">
+		<button on:click|preventDefault={() => send('?/login')}>Log In</button>
+		<div class="w-2" />
+		<button on:click|preventDefault={() => send('?/register')}>Register</button>
+	</div>
 </form>
 
-<h1>Result</h1>
-<pre>{JSON.stringify(form, undefined, 2)}</pre>
+{#if form?.error}
+	<h1 class="text-red-500">{form.error.status}: {form.error.data}</h1>
+{/if}
+
+{#if form?.result}
+	<h1 class="text-green-500">Success</h1>
+{/if}
 
 <style>
 	form > * {
 		@apply mb-2;
+	}
+
+	button {
+		@apply flex-1;
 	}
 </style>
