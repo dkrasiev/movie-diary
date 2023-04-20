d664ce125fa89b44df1191a27b52a056c762086d
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-04-20 22:06:18 +0400

    refactor: move component to lib/components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Header from './Header.svelte';
-	import Footer from './Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import './styles.css';
 	import type { LayoutServerData } from './$types';
 
