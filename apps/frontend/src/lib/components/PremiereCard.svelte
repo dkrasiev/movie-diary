e1589a3088ddd76c0b77b646524075ccc5c536b3
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-06-06 23:31:36 +0400

    feat: add animations when hover premiere card

--- apps/frontend/src/lib/components/PremiereCard.svelte
+++ apps/frontend/src/lib/components/PremiereCard.svelte
@@ -7,25 +7,35 @@
 
 	const movie = premiere.expand?.movie;
 	const name = movie?.nameRu || movie?.nameEn || movie?.nameOriginal;
-	const premiereDate = dayjs(premiere.premiereRu).locale('ru').format('D MMMM YYYY');
+	const premiereDate = dayjs(premiere.premiereRu).locale('ru').format('DD.MM.YYYY');
 </script>
 
 <a
 	style={`background-image: url('${movie?.posterUrlPreview}')`}
-	class="relative aspect-[2/3] bg-cover rounded-container-token overflow-hidden border border-surface-200"
+	class="relative aspect-[2/3] bg-cover rounded-container-token overflow-hidden border border-surface-200 transition-all"
 	href={`/premieres/${premiere.id}`}
 >
-	<span
-		class="absolute top-0 left-0 p-1 rounded-br-container-token transition-all duration-300 variant-filled-surface"
-	>
-		{name}
-		<br />
-		{`Дата выхода ${premiereDate}`}
-	</span>
+	<div class="w-full h-full">
+		<span
+			class="absolute top-0 left-0 p-1 rounded-br-container-token variant-filled-surface translate-x-0 transition duration-300"
+		>
+			{name}
+			<br />
+			{premiereDate}
+		</span>
+
+		<!-- <form class="absolute bottom-0 right-2 -translate-y-2 transition duration-300">
+			<button class="variant-filled-primary rounded p-1">Подписаться</button>
+		</form> -->
+	</div>
 </a>
 
 <style>
-	a:not(:hover) > .absolute {
-		@apply text-transparent bg-transparent border-none;
+	a:not(:hover) > div > span {
+		@apply -translate-x-[100%];
+	}
+
+	a:not(:hover) > div > form {
+		@apply translate-y-[100%];
 	}
 </style>
