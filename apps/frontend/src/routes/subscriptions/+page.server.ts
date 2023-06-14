88bcabdd2e3c4ee6438a107251e1f69c9ca366d2
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-06-14 21:18:50 +0400

    refactor(frontend): update subscription's load function

--- apps/frontend/src/routes/subscriptions/+page.server.ts
+++ apps/frontend/src/routes/subscriptions/+page.server.ts
@@ -2,8 +2,9 @@ import { SubscriptionService } from '$lib/server/services/subscription.service';
 
 export async function load({ locals }) {
 	const subscriptionService = new SubscriptionService(locals.pb);
+	const subscriptions = await subscriptionService.getSubscriptionList();
 
 	return {
-		subscriptions: structuredClone(await subscriptionService.getSubscriptionList())
+		subscriptions: structuredClone(subscriptions)
 	};
 }
